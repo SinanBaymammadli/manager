633813ecdd6bee76bb55264bed53a769cc89f19c
sinan.baymammadli <sinan.baymammadli@gmail.com>
2018-04-18 01:24:37 +0400

    added calendar screen components

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
+import React from "react";
 import { TabNavigator } from "react-navigation";
+import Icon from "react-native-vector-icons/dist/Ionicons";
 
-import Calendar from "./screens/Calendar";
+import CalendarScreen from "./screens/Calendar";
 import Home from "./screens/Home";
 import Inbox from "./screens/Inbox";
 import People from "./screens/People";
@@ -8,14 +10,80 @@ import Profile from "./screens/Profile";
 
 const RootNavigator = TabNavigator(
   {
-    Home: { screen: Home },
-    Calendar: { screen: Calendar },
-    Inbox: { screen: Inbox },
-    People: { screen: People },
-    Profile: { screen: Profile }
+    Home: {
+      screen: Home,
+      navigationOptions: ({ navigation }) => ({
+        title: "Hjem",
+        tabBarIcon: ({ focused, tintColor }) => {
+          return <Icon name="ios-home-outline" size={25} color={tintColor} />;
+        }
+      })
+    },
+    People: {
+      screen: People,
+      navigationOptions: ({ navigation }) => ({
+        title: "BORGERE",
+        tabBarIcon: ({ focused, tintColor }) => {
+          return <Icon name="ios-people-outline" size={25} color={tintColor} />;
+        }
+      })
+    },
+    CalendarScreen: {
+      screen: CalendarScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: "KALENDAR",
+        tabBarIcon: ({ focused, tintColor }) => {
+          return (
+            <Icon name="ios-calendar-outline" size={25} color={tintColor} />
+          );
+        }
+      })
+    },
+    Inbox: {
+      screen: Inbox,
+      navigationOptions: ({ navigation }) => ({
+        title: "INDBAKKE",
+        tabBarIcon: ({ focused, tintColor }) => {
+          return (
+            <Icon name="ios-chatboxes-outline" size={25} color={tintColor} />
+          );
+        }
+      })
+    },
+
+    Profile: {
+      screen: Profile,
+      navigationOptions: ({ navigation }) => ({
+        title: "PROFIL",
+        tabBarIcon: ({ focused, tintColor }) => {
+          return (
+            <Icon name="ios-settings-outline" size={25} color={tintColor} />
+          );
+        }
+      })
+    }
   },
   {
-    tabBarPosition: "bottom"
+    initialRouteName: "CalendarScreen",
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      showIcon: true,
+      showLabel: true,
+      activeTintColor: "#8bc250",
+      labelStyle: {
+        fontSize: 8,
+        margin: 0,
+        marginTop: 5,
+        fontWeight: "bold"
+      },
+      style: {
+        backgroundColor: "#fff"
+      },
+      inactiveTintColor: "#4e4e4e",
+      indicatorStyle: {
+        backgroundColor: "#8bc250"
+      }
+    }
   }
 );
 
--- src/screens/Calendar.js
+++ src/screens/Calendar.js
@@ -1,12 +1,65 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { Calendar } from "react-native-calendars";
+import Icon from "react-native-vector-icons/dist/Ionicons";
 
-export default class Calendar extends Component {
+export default class CalendarScreen extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Calendar</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.calendarTitle}>
+            <Text style={styles.calendarTitleText}>Kalender</Text>
+          </View>
+          <Calendar
+            theme={{
+              arrowColor: "#8bc250",
+              monthTextColor: "#8bc250",
+              selectedDayBackgroundColor: "#8bc250",
+              dotColor: "#8bc250",
+              todayTextColor: "#8bc250"
+            }}
+            style={{
+              padding: 10
+            }}
+            markedDates={{
+              "2018-04-18": {
+                selected: true
+              },
+              "2018-04-15": { marked: true },
+              "2018-04-20": { marked: true },
+              "2018-04-22": { marked: true },
+              "2018-04-26": { marked: true },
+              "2018-04-29": { marked: true }
+            }}
+            renderArrow={direction => (
+              <Icon
+                name={`${
+                  direction === "left" ? "ios-arrow-back" : "ios-arrow-forward"
+                }`}
+                size={20}
+                color="#8bc250"
+              />
+            )}
+          />
+
+          <View style={styles.eventContainer}>
+            <Text style={styles.eventHeader}>MINE AFTALER</Text>
+            <View style={styles.event}>
+              <View style={styles.eventTimeContainer}>
+                <Text style={styles.eventTime}>12:00</Text>
+                <Text style={styles.eventTime}>13:30</Text>
+              </View>
+              <View style={styles.eventText}>
+                <Text style={styles.eventTitle}>Mode hos AZ Web</Text>
+                <Text style={styles.eventDesc}>
+                  Produktionsvej 1, 2600 Glostrup
+                </Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -14,13 +67,46 @@ export default class Calendar extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    padding: 16,
+    backgroundColor: "#eff3f6"
   },
-  welcome: {
+  calendarTitle: {
+    width: "100%",
+    paddingHorizontal: 16,
+    paddingVertical: 20,
+    backgroundColor: "#fff"
+  },
+  calendarTitleText: {
+    fontSize: 28,
+    color: "#8bc250"
+  },
+  eventContainer: {
+    marginVertical: 15
+  },
+  eventHeader: {
+    fontSize: 16,
+    color: "#000",
+    marginVertical: 10
+  },
+  event: {
+    flexDirection: "row",
+    marginVertical: 15
+  },
+  eventTimeContainer: {
+    borderRightWidth: 2,
+    borderRightColor: "#8bc250",
+    paddingRight: 10
+  },
+  eventTime: {
+    fontSize: 25
+  },
+  eventText: {
+    flex: 1,
+    marginLeft: 10
+  },
+  eventTitle: {
+    marginTop: 10,
     fontSize: 20,
-    textAlign: "center",
-    margin: 10
+    color: "#000"
   }
 });
